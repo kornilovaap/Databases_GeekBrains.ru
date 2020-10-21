b2fc2b9ca03282805aa4d4e4dbe699237ee34d8c
kornilovaap <67694230+kornilovaap@users.noreply.github.com>
2020-10-21 12:55:17 +0300

    Update HW_lesson6.sql
    Первую задачку решить не смогла. В последней тоже не понимаю, как объединить данные по разным таблицам.

--- HW_lesson6.sql
+++ HW_lesson6.sql
@@ -1,21 +1,38 @@
 /*Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех общался с нашим пользователем.*/
-SELECT  
-	id,
-    from_user_id,
-    to_user_id,
-    group_concat(to_user_id) as a,
-    count(from_user_id) as b
-    
-FROM vk.messages
-group by from_user_id
-order by b desc;
+-- нашли всех друзей случайного пользователя id=1
+	SELECT * FROM vk.friend_requests
+	where status=1 and (from_user_id=1 or to_user_id=1);
+-- находим ве сообщения этому пользователю id=1
+	SELECT * FROM messages
+	where from_user_id=1 or to_user_id=1;
+-- находим среди всех сообщений сообщения от друзей
+	НЕ ПОНИМАЮ, КАК ЭТО СДЕЛАТЬ
 
 
 /*Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.*/
+-- 10 самых молодых пользователей
+	select user_id from profiles
+	order by birthday desc
+	limit 10;
+-- сделали из запроса выше подзапрос
+	select count(user_id) from likes 
+	where user_id in (select * from (select user_id from profiles order by birthday desc limit 10));
 
-
+					 
 /*Определить кто больше поставил лайков (всего) - мужчины или женщины?*/
+	select if(  
+		(select count(user_id) from likes
+		where user_id in (
+			select user_id from profiles where gender='m'))
+		>
+		(select count(user_id) from likes
+		where user_id in (
+			select user_id from profiles where gender='f')),
+		'больше мужчины','больше женщины');
 
-
+	 
 /*Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети.*/
-
+	select id, count(*) as count from messages
+	group by from_user_id
+	order by count
+	limit 10;
