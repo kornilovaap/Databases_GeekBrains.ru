02414f264c5352fd6af812e4d010ccc365b15985
kornilovaap <67694230+kornilovaap@users.noreply.github.com>
2020-10-29 13:46:37 +0300

    Update HW_lesson11.sql

--- HW_lesson11.sql
+++ HW_lesson11.sql
@@ -1,9 +1,48 @@
 /*Создайте таблицу logs типа Archive. 
 Пусть при каждом создании записи в таблицах users, catalogs и products в таблицу logs помещается время и дата создания записи, 
 название таблицы, идентификатор первичного ключа и содержимое поля name.*/
+CREATE TABLE `shop`.`logs` (
+  `apdate_at` DATETIME NOT NULL,
+  `tbl_name` VARCHAR(45) NOT NULL,
+  `id` BIGINT NOT NULL,
+  `description_name` VARCHAR(245) NOT NULL)
+ENGINE = ARCHIVE;
+
+-- для USERS
+DROP TRIGGER IF EXISTS `shop`.`users_AFTER_INSERT`;
+DELIMITER $$
+USE `vk`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `shop`.`users_AFTER_INSERT` AFTER INSERT ON `users` FOR EACH ROW
+BEGIN
+	insert into `shop`.`logs` (apdate_at,tbl_name,id,description_name)
+    values (now(), `users`, new.id, new.name);
+END$$
+DELIMITER ;
+
+-- для catalogs
+DROP TRIGGER IF EXISTS `shop`.`catalogs_AFTER_INSERT`;
+DELIMITER $$
+USE `shop`$$
+CREATE DEFINER=`root`@`localhost` TRIGGER `catalogs_AFTER_INSERT` AFTER INSERT ON `catalogs` FOR EACH ROW 
+BEGIN
+insert into `shop`.`logs` (apdate_at, tbl_name, id, description_name)
+    values (now(), `catalogs`, new.id, new.name);
+END$$
+DELIMITER ;
+
+-- для products
+DROP TRIGGER IF EXISTS `shop`.`products_AFTER_INSERT`;
+DELIMITER $$
+USE `shop`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `shop`.`products_AFTER_INSERT` AFTER INSERT ON `products` FOR EACH ROW
+BEGIN
+ insert into `shop`.`logs` (apdate_at, tbl_name, id, description_name)
+    values (now(), `products`, new.id, new.name);
+END$$
+DELIMITER ;
 
 
-/*(по желанию) Создайте SQL-запрос, который помещает в таблицу users миллион записей.*/
+/*Создайте SQL-запрос, который помещает в таблицу users миллион записей.*/
 
 
 /*В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов*/
