fe8fe9a62e5849589f2c30a44d41c90ba7d95793
kornilovaap <67694230+kornilovaap@users.noreply.github.com>
2020-10-29 14:02:32 +0300

    Update HW_lesson11.sql

--- HW_lesson11.sql
+++ HW_lesson11.sql
@@ -43,8 +43,23 @@ DELIMITER ;
 
 
 /*Создайте SQL-запрос, который помещает в таблицу users миллион записей.*/
-
-
+USE `shop`;
+DROP procedure IF EXISTS `insert_one_million`;
+DELIMITER $$
+USE `shop`$$
+CREATE PROCEDURE `insert_one_million` ()
+BEGIN
+ declare i int default 10;
+ declare j int default 0;
+ while i> 100 do
+	insert into `test` values(j, now());
+	set i =i-1;
+        set j=j+1;
+ end while;
+END$$
+DELIMITER ;
+				  
+				  
 /*В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов*/
 
 
