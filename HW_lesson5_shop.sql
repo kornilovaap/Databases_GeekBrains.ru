8b3de96fe08e9d01f53456d66ca9162f091816c5
kornilovaap <67694230+kornilovaap@users.noreply.github.com>
2020-11-10 13:43:41 +0300

    Update HW_lesson5_shop.sql

--- HW_lesson5_shop.sql
+++ HW_lesson5_shop.sql
@@ -29,6 +29,7 @@ ALTER TABLE users_hw MODIFY created_at DATETIME, MODIFY updated_at DATETIME;
 /*В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0, если товар закончился и выше нуля, если на складе имеются запасы. Необходимо отсортировать записи таким образом, чтобы они выводились в порядке увеличения значения value. Однако нулевые запасы должны выводиться в конце, после всех */
 insert into storehouses_products (value) values (11), (0),(0),(3),(5),(8);
 sselect * from storehouses_products ORDER BY CASE WHEN value = 0 then 999999999 else value end;
+-- Ответ препода: Интересное решение =) Можно было так: ORDER BY IF(value>0, 0, 1), value
 
 
 /*Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае. Месяцы заданы в виде списка английских названий (may, august)*/
