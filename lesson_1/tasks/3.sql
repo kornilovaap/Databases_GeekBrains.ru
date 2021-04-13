0638832008e57c5ec7fe7178e6b2e315920f3cf1
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-04-13 14:36:40 +0300

    Create 3.sql

---
+++ lesson_1/tasks/3.sql
@@ -0,0 +1,9 @@
+/* 3) Создайте дамп единственной таблицы help_keyword базы данных mysql. Причем добейтесь того, чтобы дамп содержал только первые 100 строк таблицы */
+
+create database keydump; -- сделали пустую бд для будущего дампа
+\q -- вышли из скуля, чтобы делать дамп
+mysqldump --where="true limit 100" mysql help_keyword > qwerty.sql -- скопировали 
+mysql keydump < qwerty.sql
+-- ERROR 3723 (HY000) at line 25: The table 'help_keyword' may not be created in the reserved tablespace 'mysql'
+-- я попробовала. не получилось (((
+     
