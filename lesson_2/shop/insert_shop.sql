f009248f432d6aedb96c59731dd55dac499fc039
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-04-14 13:09:29 +0300

    Update insert_shop.sql

--- lesson_2/shop/insert_shop.sql
+++ lesson_2/shop/insert_shop.sql
@@ -1,6 +1,12 @@
-INSERT INTO catalogs (name) VALUES ('Процессоры');
+/* INSERT INTO catalogs (name) VALUES ('Процессоры');
 INSERT INTO catalogs VALUES (0, 'Мат.платы');
-INSERT INTO catalogs VALUES (NULL, 'Видекарты');
+INSERT INTO catalogs VALUES (NULL, 'Видекарты');  */
+
+INSERT INTO catalogs VALUES
+  (DEFAULT, 'Процессоры'),
+  (DEFAULT, 'Мат.платы'),
+  (DEFAULT, 'Видеокарты');
+  
 SELECT * FROM catalogs;
 >>>
 +----+------------+
