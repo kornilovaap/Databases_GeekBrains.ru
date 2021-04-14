c78b25936d19b481126804ac0e7f4a7149d26683
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-04-14 13:47:45 +0300

    Update 3.sql

--- lesson_2/tasks/3.sql
+++ lesson_2/tasks/3.sql
@@ -1,9 +1,25 @@
-/* 3) Создайте дамп единственной таблицы help_keyword базы данных mysql. Причем добейтесь того, чтобы дамп содержал только первые 100 строк таблицы */
+/* В учебной базе данных shop присутствует таблица catalogs. 
+Пусть в базе данных sample имеется таблица cat, в которой могут присутствовать строки с такими же первичными ключами. 
+Напишите запрос, который копирует данные из таблицы catalogs в таблицу cat, 
+при этом для записей с конфликтующими первичными ключами в таблице cat должна производиться замена значениями из таблицы catalogs.*/
 
-create database keydump; -- сделали пустую бд для будущего дампа
-\q -- вышли из скуля, чтобы делать дамп
-mysqldump --where="true limit 100" mysql help_keyword > qwerty.sql -- скопировали 
-mysql keydump < qwerty.sql
--- ERROR 3723 (HY000) at line 25: The table 'help_keyword' may not be created in the reserved tablespace 'mysql'
--- я попробовала. не получилось (((
-    
+use shop;
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет магазина';
+
+use example;
+DROP TABLE IF EXISTS cat;
+CREATE TABLE cat (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) 
+) ;
+
+insert into cat value (1, 'TV');
+
+INSERT INTO cat
+	SELECT * FROM shop.catalogs;
+SELECT * FROM cat;
