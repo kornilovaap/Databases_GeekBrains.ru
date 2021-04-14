a9fe7fc5e8784df189831b2a43b8ada639d15736
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-04-14 13:07:08 +0300

    Update create_shop.sql

--- lesson_2/shop/create_shop.sql
+++ lesson_2/shop/create_shop.sql
@@ -1,63 +1,67 @@
 DROP TABLE IF EXISTS catalogs;
 CREATE TABLE catalogs (
-  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Название раздела'
-  /*PRIMARY KEY(id, name(10))  --индекс не обязательно должен быть объявлен по одному столбцу. тут id + первые 10 символов столбца*/
 ) COMMENT = 'Разделы интернет магазина';
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Имя покупателя',
   birthday_at DATE COMMENT 'Дата рождения',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,   -- без этого будет автоматически присваиваться текущая дата
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP --для автоматического обновления даты
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- для автоматического обновления даты
 ) COMMENT = 'Покупатели';
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
-  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Название',
   desription TEXT COMMENT 'Описание',
   price DECIMAL (11,2) COMMENT 'Цена',
   catalog_id INT UNSIGNED,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
 ) COMMENT = 'Товарные позиции';
+
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   user_id INT UNSIGNED,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
 ) COMMENT = 'Заказы';
 
 DROP TABLE IF EXISTS orders_products;
 CREATE TABLE orders_products (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   order_id INT UNSIGNED,
   product_id INT UNSIGNED,
-  total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',  --подставляется значение 1, если др не задано
+  total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',  -- подставляется значение 1, если др не задано
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) COMMENT = 'Состав заказа';
 
 DROP TABLE IF EXISTS discounts;
 CREATE TABLE discounts (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   user_id INT UNSIGNED,
   product_id INT UNSIGNED,
   discount FLOAT UNSIGNED COMMENT 'Величина скидки от 0.0 до 1.0',
   finished_at DATETIME NULL,
   started_at DATETIME NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id),
+  KEY index_of_product_id(product_id)
 ) COMMENT = 'Скидки';
 
 DROP TABLE IF EXISTS storehouses;
 CREATE TABLE storehouses (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Название',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -65,7 +69,7 @@ CREATE TABLE storehouses (
 
 DROP TABLE IF EXISTS storehouses_products;
 CREATE TABLE storehouses_products (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   storehouse_id INT UNSIGNED,
   product_id INT UNSIGNED,
   value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
