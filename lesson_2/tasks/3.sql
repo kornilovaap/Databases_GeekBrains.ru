84aa079327562893b80035b401725e9988433522
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-04-14 14:06:23 +0300

    on duplicate key update
    on duplicate key update

--- lesson_2/tasks/3.sql
+++ lesson_2/tasks/3.sql
@@ -10,6 +10,19 @@ CREATE TABLE catalogs (
   name VARCHAR(255) COMMENT 'Название раздела',
   UNIQUE unique_name(name(10))
 ) COMMENT = 'Разделы интернет магазина';
+INSERT INTO catalogs VALUES
+  (DEFAULT, 'Процессоры'),
+  (DEFAULT, 'Мат.платы'),
+  (DEFAULT, 'Видеокарты');
+select * from catalogs;
++----+------------+
+| id | name       |
++----+------------+
+|  1 | Процессоры |
+|  2 | Мат.платы  |
+|  3 | Видеокарты |
++----+------------+
+		     
 
 use example;
 DROP TABLE IF EXISTS cat;
@@ -17,9 +30,27 @@ CREATE TABLE cat (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) 
 ) ;
-
 insert into cat value (1, 'TV');
+select * from cat;
++----+------------+
+| id | name       |
++----+------------+
+|  1 | TV         |
+|  2 | Мат.платы  |
+|  3 | Видеокарты |
++----+------------+
 
+		     
 INSERT INTO cat
-	SELECT * FROM shop.catalogs;
+    SELECT * FROM shop.catalogs
+    on duplicate key update id = values(id), name = values(name);
+    
 SELECT * FROM cat;
++----+------------+
+| id | name       |
++----+------------+
+|  1 | Процессоры |
+|  2 | Мат.платы  |
+|  3 | Видеокарты |
++----+------------+
+		         
