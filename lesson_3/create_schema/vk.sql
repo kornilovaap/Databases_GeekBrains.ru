dae3d5c97dc6c3f966a3a1a7a07b6de0a3329510
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-04-15 17:33:09 +0300

    Update vk.sql

--- lesson_3/create_schema/vk.sql
+++ lesson_3/create_schema/vk.sql
@@ -1,190 +1,303 @@
--- MySQL Workbench Synchronization
--- Generated: 2020-09-29 11:25
--- Model: New Model
--- Version: 1.0
--- Project: Name of the project
--- Author: v
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: vkontakte
+-- ------------------------------------------------------
+-- Server version	8.0.23
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE IF NOT EXISTS `vk`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `firstname` VARCHAR(245) NOT NULL COMMENT 'имя',
-  `lastname` VARCHAR(245) NOT NULL COMMENT 'фамилия',
-  `email` VARCHAR(245) NOT NULL,
-  `phone` BIGINT(11) NOT NULL COMMENT 'телефон',
-  `password_hash` CHAR(64) NOT NULL,
+--
+-- Table structure for table `communities`
+--
+
+DROP TABLE IF EXISTS `communities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `communities` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(245) NOT NULL,
+  `adm_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  UNIQUE INDEX `phone_UNIQUE` (`phone` ASC) VISIBLE,
-  INDEX `firstname_lastname_idx` (`lastname` ASC, `firstname` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci
-COMMENT = 'пользователи';
+  KEY `fk_communities_users1_idx` (`adm_id`),
+  CONSTRAINT `fk_communities_users1` FOREIGN KEY (`adm_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `vk`.`profiles` (
-  `users_id` INT(11) NOT NULL,
-  `gender` ENUM('m', 'f', 'x') NOT NULL,
-  `birthday` DATE NOT NULL,
-  `location` VARCHAR(245) NULL DEFAULT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT NOW(),
-  INDEX `fk_profiles_users_idx` (`users_id` ASC) VISIBLE,
+--
+-- Dumping data for table `communities`
+--
+
+LOCK TABLES `communities` WRITE;
+/*!40000 ALTER TABLE `communities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `communities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `friend_requests`
+--
+
+DROP TABLE IF EXISTS `friend_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `friend_requests` (
+  `from_uses_id` int NOT NULL AUTO_INCREMENT,
+  `to_user_id` int NOT NULL,
+  `status` tinyint unsigned NOT NULL COMMENT 'статус: запрос - 1, подтвержден - 2, отклонен - 0',
+  PRIMARY KEY (`from_uses_id`,`to_user_id`),
+  KEY `fk_friend_requests_users1_idx` (`from_uses_id`),
+  KEY `fk_friend_requests_users2_idx` (`to_user_id`),
+  CONSTRAINT `fk_friend_requests_users1` FOREIGN KEY (`from_uses_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_friend_requests_users2` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friend_requests`
+--
+
+LOCK TABLES `friend_requests` WRITE;
+/*!40000 ALTER TABLE `friend_requests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `friend_requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `media` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `media_type_id` int NOT NULL,
+  `text` text,
+  `file` blob,
+  `size` int DEFAULT NULL,
+  `metadata` json DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_media_media_types1_idx` (`media_type_id`),
+  CONSTRAINT `fk_media_media_types1` FOREIGN KEY (`media_type_id`) REFERENCES `media_types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media`
+--
+
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media_types`
+--
+
+DROP TABLE IF EXISTS `media_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `media_types` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(245) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media_types`
+--
+
+LOCK TABLES `media_types` WRITE;
+/*!40000 ALTER TABLE `media_types` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `from_uses_id` int NOT NULL,
+  `to_user_id` int NOT NULL,
+  `text` text,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_messages_users1_idx` (`from_uses_id`),
+  KEY `fk_messages_users2_idx` (`to_user_id`),
+  CONSTRAINT `fk_messages_users1` FOREIGN KEY (`from_uses_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_messages_users2` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `photo`
+--
+
+DROP TABLE IF EXISTS `photo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `photo` (
+  `id` int NOT NULL,
+  `album_id` int NOT NULL,
+  `media_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_photo_photo_album1_idx` (`album_id`),
+  KEY `fk_photo_media1_idx` (`media_id`),
+  CONSTRAINT `fk_photo_media1` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`),
+  CONSTRAINT `fk_photo_photo_album1` FOREIGN KEY (`album_id`) REFERENCES `photo_album` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `photo`
+--
+
+LOCK TABLES `photo` WRITE;
+/*!40000 ALTER TABLE `photo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `photo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `photo_album`
+--
+
+DROP TABLE IF EXISTS `photo_album`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `photo_album` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(245) NOT NULL,
+  `description` text,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_photo_album_users1_idx` (`user_id`),
+  CONSTRAINT `fk_photo_album_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `photo_album`
+--
+
+LOCK TABLES `photo_album` WRITE;
+/*!40000 ALTER TABLE `photo_album` DISABLE KEYS */;
+/*!40000 ALTER TABLE `photo_album` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `profile` (
+  `users_id` int NOT NULL,
+  `gender` enum('m','f','x') NOT NULL,
+  `birthday` date NOT NULL,
+  `location` varchar(245) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`users_id`),
-  CONSTRAINT `fk_profiles_users`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_profile_users_idx` (`users_id`),
+  CONSTRAINT `fk_profile_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `vk`.`friend_requests` (
-  `from_user_id` INT(11) NOT NULL,
-  `to_user_id` INT(11) NOT NULL,
-  `status` TINYINT(1) UNSIGNED NOT NULL COMMENT 'статус: 0 - запрос, 1 - подтвержден, 2 - отклонен',
-  INDEX `fk_friend_requests_users1_idx` (`from_user_id` ASC) VISIBLE,
-  INDEX `fk_friend_requests_users2_idx` (`to_user_id` ASC) VISIBLE,
-  PRIMARY KEY (`from_user_id`, `to_user_id`),
-  CONSTRAINT `fk_friend_requests_users1`
-    FOREIGN KEY (`from_user_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_friend_requests_users2`
-    FOREIGN KEY (`to_user_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Dumping data for table `profile`
+--
 
-CREATE TABLE IF NOT EXISTS `vk`.`messages` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `from_user_id` INT(11) NOT NULL,
-  `to_user_id` INT(11) NOT NULL,
-  `text` TEXT NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT NOW(),
+LOCK TABLES `profile` WRITE;
+/*!40000 ALTER TABLE `profile` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profile` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(245) NOT NULL COMMENT 'Имя',
+  `last_name` varchar(245) NOT NULL COMMENT 'Фамилия',
+  `email` varchar(245) NOT NULL COMMENT 'Электронная почта',
+  `phone` bigint NOT NULL COMMENT 'Сотовый телефон',
+  `password_hash` char(64) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_messages_users1_idx` (`from_user_id` ASC) VISIBLE,
-  INDEX `fk_messages_users2_idx` (`to_user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_messages_users1`
-    FOREIGN KEY (`from_user_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_messages_users2`
-    FOREIGN KEY (`to_user_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `phone_UNIQUE` (`phone`),
+  KEY `first_name_last_name_idx` (`last_name`,`first_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `vk`.`communities` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(245) NOT NULL,
-  `admin_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_communities_users1_idx` (`admin_id` ASC) VISIBLE,
-  CONSTRAINT `fk_communities_users1`
-    FOREIGN KEY (`admin_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Dumping data for table `users`
+--
 
-CREATE TABLE IF NOT EXISTS `vk`.`users_communities` (
-  `community_id` INT(11) NOT NULL,
-  `user_id` INT(11) NOT NULL,
-  INDEX `fk_users_communities_users1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_users_communities_communities1_idx` (`community_id` ASC) VISIBLE,
-  PRIMARY KEY (`community_id`, `user_id`),
-  CONSTRAINT `fk_users_communities_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_communities_communities1`
-    FOREIGN KEY (`community_id`)
-    REFERENCES `vk`.`communities` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `vk`.`media_types` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(245) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Table structure for table `users_communutuies`
+--
 
-CREATE TABLE IF NOT EXISTS `vk`.`media` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `media_type_id` INT(11) NOT NULL,
-  `text` TEXT NULL DEFAULT NULL,
-  `file` BLOB NULL DEFAULT NULL COMMENT '/files/img/01.jpg',
-  `size` INT(11) NULL DEFAULT NULL,
-  `metadata` JSON NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_media_media_types1_idx` (`media_type_id` ASC) VISIBLE,
-  CONSTRAINT `fk_media_media_types1`
-    FOREIGN KEY (`media_type_id`)
-    REFERENCES `vk`.`media_types` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `users_communutuies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_communutuies` (
+  `user_id` int NOT NULL,
+  `community_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`community_id`),
+  KEY `fk_users_communutuies_users1_idx` (`user_id`),
+  KEY `fk_users_communutuies_communities1_idx` (`community_id`),
+  CONSTRAINT `fk_users_communutuies_communities1` FOREIGN KEY (`community_id`) REFERENCES `communities` (`id`),
+  CONSTRAINT `fk_users_communutuies_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `vk`.`photo_albums` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(245) NOT NULL,
-  `decs` TEXT NULL DEFAULT NULL,
-  `user_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_photo_albums_users1_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_photo_albums_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `vk`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Dumping data for table `users_communutuies`
+--
 
-CREATE TABLE IF NOT EXISTS `vk`.`photos` (
-  `id` INT(11) NOT NULL,
-  `album_id` INT(11) NOT NULL,
-  `media_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_photos_photo_albums1_idx` (`album_id` ASC) VISIBLE,
-  INDEX `fk_photos_media1_idx` (`media_id` ASC) VISIBLE,
-  CONSTRAINT `fk_photos_photo_albums1`
-    FOREIGN KEY (`album_id`)
-    REFERENCES `vk`.`photo_albums` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_photos_media1`
-    FOREIGN KEY (`media_id`)
-    REFERENCES `vk`.`media` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+LOCK TABLES `users_communutuies` WRITE;
+/*!40000 ALTER TABLE `users_communutuies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_communutuies` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Dump completed on 2021-04-15 17:31:36
